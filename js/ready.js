ec261725b773d88a2ed2382731b59f1d7fbd24f4
Alfredahoy <Alfred.dj.hoi@gmail.com>
2018-07-29 10:39:49 +1200

    v.2.0.8

--- js/ready.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-        $('.circle').addClass('clicked');
-        $('.nav').toggleClass('open');
-        $('html').toggleClass('menu');
-        $('main').toggleClass('open');
-        $('.loading-screen').toggleClass('open');
-        $('#background').toggleClass('open');
-}); 
